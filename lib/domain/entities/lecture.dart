cc12b122e12c71daf9d25baf70255c4428a48922
dimassdoubles <codedimas@gmail.com>
2022-10-09 04:42:16 +0700

    implement encapsulation

--- lib/domain/entities/lecture.dart
+++ lib/domain/entities/lecture.dart
@@ -1,5 +1,14 @@
 class Lecture {
-  String title;
-  String videoLink;
-  Lecture({required this.title, required this.videoLink});
+  String _title, _videoLink;
+  Lecture({required String title, required String videoLink})
+      : _title = title,
+        _videoLink = videoLink;
+
+  String get title {
+    return _title;
+  }
+
+  String get videoLink {
+    return _videoLink;
+  }
 }
