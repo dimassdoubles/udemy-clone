d8bb7bc483f7003346b2c4d1357daeab55d6f531
dimassdoubles <codedimas@gmail.com>
2022-10-09 02:47:02 +0700

    modify and add properties
    change sections to curciculum

--- lib/domain/entities/course.dart
+++ lib/domain/entities/course.dart
@@ -1,25 +1,68 @@
+import 'package:udemy_clone/domain/entities/section.dart';
+
 class Course {
-  final String _title, _instructor, _coverUrl;
-  final List<String> _tags;
+  final String _title, _instructor, _coverUrl, _description, _courseId;
+  final int _price, _reviewsCount, _lecturesCount;
+  final List<String> _tags, _whatWillLearn;
   final bool _isOnlyWeb;
   final double _rating;
-  final int _reviewsCount;
+  final List<Section> _curiculums;
 
   Course({
+    required String description,
+    required String courseId,
     required String title,
     required String instructor,
     required String coverUrl,
     required double rating,
     required int reviewsCount,
-    bool isOnlyWeb = true,
+    required int lecturesCount,
+    required int price,
+    required List<String> whatWillLearn,
     List<String> tags = const [],
-  })  : _title = title,
+    required List<Section> curiculums,
+    bool isOnlyWeb = true,
+  })  : _description = description,
+        _price = price,
+        _whatWillLearn = whatWillLearn,
+        _courseId = courseId,
+        _title = title,
         _instructor = instructor,
         _coverUrl = coverUrl,
         _rating = rating,
         _reviewsCount = reviewsCount,
         _isOnlyWeb = isOnlyWeb,
-        _tags = tags;
+        _tags = tags,
+        _curiculums = curiculums,
+        _lecturesCount = lecturesCount;
+
+  String get courseId {
+    return _courseId;
+  }
+
+  List<String> get whatWillLearn {
+    return _whatWillLearn;
+  }
+
+  int get price {
+    return _price;
+  }
+
+  String get courseId {
+    return _courseId;
+  }
+
+  String get description {
+    return _description;
+  }
+
+  int get lecturesCount {
+    return _lecturesCount;
+  }
+
+  List<Section> get curiculums {
+    return _curiculums;
+  }
 
   String get title {
     return _title;
