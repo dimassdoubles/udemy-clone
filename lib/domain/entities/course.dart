27be0aece9d0f6153ba09492ab9b7e4bbf5ae549
dimassdoubles <codedimas@gmail.com>
2022-10-08 23:11:43 +0700

    implementation encapsulation, owner to instructor

--- lib/domain/entities/course.dart
+++ lib/domain/entities/course.dart
@@ -1,17 +1,51 @@
 class Course {
-  String title, owner, coverUrl;
-  List<String> tags;
-  bool onlyWeb;
-  double rating;
-  int reviewsCount;
+  final String _title, _instructor, _coverUrl;
+  final List<String> _tags;
+  final bool _isOnlyWeb;
+  final double _rating;
+  final int _reviewsCount;
 
   Course({
-    required this.title,
-    required this.owner,
-    required this.coverUrl,
-    required this.rating,
-    required this.reviewsCount,
-    this.onlyWeb = true,
-    this.tags = const [],
-  });
+    required String title,
+    required String instructor,
+    required String coverUrl,
+    required double rating,
+    required int reviewsCount,
+    bool isOnlyWeb = true,
+    List<String> tags = const [],
+  })  : _title = title,
+        _instructor = instructor,
+        _coverUrl = coverUrl,
+        _rating = rating,
+        _reviewsCount = reviewsCount,
+        _isOnlyWeb = isOnlyWeb,
+        _tags = tags;
+
+  String get title {
+    return _title;
+  }
+
+  String get instructor {
+    return _instructor;
+  }
+
+  String get coverUrl {
+    return _coverUrl;
+  }
+
+  List<String> get tags {
+    return _tags;
+  }
+
+  bool get isOnlyWeb {
+    return _isOnlyWeb;
+  }
+
+  double get rating {
+    return _rating;
+  }
+
+  int get reviewsCount {
+    return _reviewsCount;
+  }
 }
