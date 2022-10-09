96b8d1df7a01b9676e866874c63ff1848336ffdc
dimassdoubles <codedimas@gmail.com>
2022-10-09 23:26:46 +0700

    add navigation
    add button to navigate to playerPage

--- lib/presentation/pages/with_bottom_navbar/learning_page.dart
+++ lib/presentation/pages/with_bottom_navbar/learning_page.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 
+import '../../../share/routes.dart';
+
 class LearningPage extends StatelessWidget {
   const LearningPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-        child: Text('Learning Page'),
-      );
+    return Center(
+      child: OutlinedButton(
+        onPressed: () {
+          Navigator.pushNamed(context, playerPage);
+        },
+        child: const Text('Go to player page'),
+      ),
+    );
   }
 }
