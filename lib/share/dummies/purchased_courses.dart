bc6549b1893e095fdd15d0005e530355d939e547
dimassdoubles <codedimas@gmail.com>
2022-10-10 01:26:24 +0700

    initial commit

---
+++ lib/share/dummies/purchased_courses.dart
@@ -0,0 +1,8 @@
+import 'package:udemy_clone/domain/entities/purchased_course.dart';
+
+List<PurchasedCourse> purchasedCourses = [
+  PurchasedCourse(
+    id: "YbyyZb9gyYM65PNlJVgB",
+    idCourse: "YZtOPn6YEFW3hnnsbyH8",
+  ),
+];
