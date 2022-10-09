c3d84667a5d375af4b2c27db435f47b2e92f5077
dimassdoubles <codedimas@gmail.com>
2022-10-09 21:43:11 +0700

    move file location

--- lib/share/routes.dart
+++ lib/share/routes.dart
@@ -3,28 +3,27 @@ import 'package:flutter/material.dart';
 import 'package:udemy_clone/presentation/pages/category_list_page.dart';
 import 'package:udemy_clone/presentation/pages/with_bottom_navbar/learning_page.dart';
 import 'package:udemy_clone/presentation/pages/with_bottom_navbar/main_page.dart';
-import 'package:udemy_clone/presentation/pages/login_page.dart';
+import 'package:udemy_clone/presentation/pages/guest_page.dart';
 import 'package:udemy_clone/presentation/pages/with_bottom_navbar/search_page.dart';
 import 'package:udemy_clone/presentation/pages/player_page.dart';
 import 'package:udemy_clone/presentation/pages/splash_page.dart';
 import 'package:udemy_clone/presentation/pages/with_bottom_navbar/wishlist_page.dart';
 
-import '../../presentation/pages/course_detail_page.dart';
-import '../../presentation/pages/course_list_page.dart';
-import '../../presentation/pages/with_bottom_navbar/account_page.dart';
-import '../../presentation/pages/signin_page.dart';
-import '../../theme_test.dart';
+import '../presentation/pages/course_list_page.dart';
+import '../presentation/pages/with_bottom_navbar/account_page.dart';
+import '../presentation/pages/signin_page.dart';
+import '../theme_test.dart';
 
 const String mainPage = "main";
 const String splashPage = "splash";
-const String loginPage = "login";
+const String guestPage = "login";
 const String signinPage = "signin";
 const String featuredPage = "home";
 const String searchPage = "search";
 const String learningPage = "learning";
 const String wishlistPage = "wishlist";
 const String accountPage = "account";
-const String courseDetailPage = "course";
+// const String courseDetailPage = "course";
 const String playerPage = "player";
 const String themeTest = "themeTest";
 const String courseListPage = "courseListPage";
@@ -49,17 +48,17 @@ Route<dynamic> controller(RouteSettings settings) {
       return MaterialPageRoute(
         builder: (context) => const PlayerPage(),
       );
-    case courseDetailPage:
-      return MaterialPageRoute(
-        builder: (context) => const CourseDetailPage(),
-      );
+    // case courseDetailPage:
+    //   return MaterialPageRoute(
+    //     builder: (context) => const CourseDetailPage(),
+    //   );
     case splashPage:
       return MaterialPageRoute(
         builder: (context) => const SplashPage(),
       );
-    case loginPage:
+    case guestPage:
       return MaterialPageRoute(
-        builder: (context) => const LoginPage(),
+        builder: (context) => const GuestPage(),
       );
     case signinPage:
       return MaterialPageRoute(
